a5d31188cf998bf85ed4ee0187e9f517b1e9af63
Matt Teichman <teichman@uchicago.edu>
2024-01-30 21:19:20 -0600

    no more PV type variables

--- lib/bar.ml
+++ lib/bar.ml
@@ -2,6 +2,7 @@ type error = Bar_intf.error
 
 let is_two_or_error x =
   let open Trace in
-  trycatch
-    (`BadBad "Foo function failed")
-    (Foo.is_two_or_error x)
+  let err = (`BadBad "Foo function failed"
+             : error :> Trace_intf.error)
+  in
+  trycatch err (Foo.is_two_or_error x)
--- lib/foo.ml
+++ lib/foo.ml
@@ -3,5 +3,6 @@ type error = Foo_intf.error
 let is_two_or_error x =
   if x = 2
   then Trace.pure x
-  else let e = `NotGood "Not given 2" in
-       Error [(e : error :> Trace_intf.error)]
+  else let err = (`NotGood "Not given 2"
+                  : error :> Trace_intf.error)
+       in Error [err]
