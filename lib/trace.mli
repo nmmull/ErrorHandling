90ecd937d45e6d0314403ece511b3fd4873f455f
Matt Teichman <teichman@uchicago.edu>
2024-02-07 14:35:35 -0600

    formatting

--- lib/trace.mli
+++ lib/trace.mli
@@ -1,9 +1,9 @@
 module Errlist : sig type t end
 
-module Make :
-functor (C : Trace_intf.COERCE) ->
-Trace_intf.TRACE with type error = C.error
-                  and type 'a trace = ('a, Errlist.t) result
+module Make : functor (C : Trace_intf.COERCE) ->
+              Trace_intf.TRACE
+              with type error = C.error
+               and type 'a trace = ('a, Errlist.t) result
 
 val export : ('a, Errlist.t) result ->
              ('a, Trace_intf.Errlist.t) result
