d19a2cac2bc0ddb9a5a76d1e7b4fc92acb53cb33
Matt Teichman <teichman@uchicago.edu>
2024-02-09 09:16:31 -0600

    hmm, will this work?  let's see

--- lib/bar.ml
+++ lib/bar.ml
@@ -1,13 +1,14 @@
-module type HIDDEN = sig
-  val trycatch : Bar_intf.error ->
-                 ('a, Trace.Errlist.t) result ->
-                 ('a, Trace.Errlist.t) result
-end
+module type ERR = sig type error end
 
-module T : HIDDEN = Trace.T
+module Specialize (E : ERR) = struct
+  type 'a trycatch = E.error -> ('a, Trace.Errlist.t) result -> ('a, Trace.Errlist.t) result
+  end
+
+type 'a trycatch = 'a Specialize(Bar_intf).trycatch
+
+let trycatch : 'a trycatch = Trace.T.trycatch
 
 let is_two_or_error x =
-  let open T in
   trycatch
     (`BadBad "Foo function failed")
     (Foo.is_two_or_error x)
